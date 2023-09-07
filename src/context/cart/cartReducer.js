6e72c0ce23489cd9729333a23fba56cfb60166dd
IG <iikim2511@gmail.com>
2023-09-07 14:58:32 -0400

    bug fix- add_to_cart

--- src/context/cart/cartReducer.js
+++ src/context/cart/cartReducer.js
@@ -1,24 +1,44 @@
 // cartReducer.js
+
+// Define the initial state
 export const initialState = {
   cartItems: [],
 };
 
+// Create the cart reducer function
 export const cartReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_TO_CART':
-      // Add product to the cart
-      return {
-        ...state,
-        cartItems: [...state.cartItems, action.payload],
-      };
+      // Check if the product is already in the cart
+      const existingItem = state.cartItems.find(item => item.id === action.payload.id);
+
+      if (existingItem) {
+        // If the item exists, increment its quantity
+        return {
+          ...state,
+          cartItems: state.cartItems.map(item =>
+            item.id === action.payload.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          ),
+        };
+      } else {
+        // If the item doesn't exist, add it to the cart with a quantity of 1
+        return {
+          ...state,
+          cartItems: [...state.cartItems, { ...action.payload, quantity: 1 }],
+        };
+      }
+
     case 'REMOVE_FROM_CART':
-      // Remove product from the cart
+      // Remove a product from the cart
       return {
         ...state,
         cartItems: state.cartItems.filter(item => item.id !== action.payload.id),
       };
+
     case 'INCREMENT_QUANTITY':
-      // Increment product quantity in the cart
+      // Increment the quantity of a product in the cart
       return {
         ...state,
         cartItems: state.cartItems.map(item =>
@@ -27,8 +47,9 @@ export const cartReducer = (state, action) => {
             : item
         ),
       };
+
     case 'DECREMENT_QUANTITY':
-      // Decrement product quantity in the cart
+      // Decrement the quantity of a product in the cart
       return {
         ...state,
         cartItems: state.cartItems.map(item =>
@@ -37,6 +58,7 @@ export const cartReducer = (state, action) => {
             : item
         ),
       };
+
     default:
       return state;
   }
